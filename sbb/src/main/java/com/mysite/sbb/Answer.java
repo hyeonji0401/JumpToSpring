bfe9cb424ea9078c1368390ae65db595a8d7bfd9
ghdgu <ghdguswl13@gmail.com>
2023-10-06 15:15:48 +0900

    엔티티 생성

---
+++ sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createData;
+
+    @ManyToOne
+    //N:1의 관계 부모는 question 자식은 answer
+    private Question question;
+}
---
+++ sbb/src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+//데이터베이스 테이블과 매핑되는 자바 클래스를 말함
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //generatedValue-자동으로 1씩 증가
+    //strategy-고유번호 생성
+    //generationType.IDENTITY-해당 컬럼만의 독립적인 시퀀스를 생성하여 번호를 증가시킴
+    private Integer id;
+
+    @Column(length =200)
+    //length-컬럼의 길이 설정
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    //columnDefinition-컬럼 속성 정의
+    //TEXT-글자 수를 제한 할 수 없는 경우
+    private String content;
+
+    private LocalDateTime createData;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    //mappedBy-참조 엔티티의 속성명(여기서는 answer엔티티에서 question 엔티티를 참조했다는 말임)
+    //casacedType.REMOVE-질문을 삭제하면 그에 달린 답변들도 모두 함꼐 삭제
+    private List<Answer> answerList;
+}
