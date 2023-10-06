28c8b3347dfdc6f4d73d871eb29316708cd9902a
ghdgu <ghdguswl13@gmail.com>
2023-10-06 14:04:02 +0900

    스프링 부트 기본 요소: 컨트롤러

---
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    //url 요청에 대한 응답으로 문자열을 리턴하라는 의미
+    @ResponseBody
+    public String index(){
+        //return 값이 없을 시 오류 발생
+        return "안녕하세요 sbb에 오신것을 환영합니다";
+    }
+}
