bd1acde5bc345062593a585d6e4623faa76bbad8
ghdgu <ghdguswl13@gmail.com>
2023-10-06 16:21:23 +0900

    findBySubject

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -39,11 +39,17 @@ class SbbApplicationTests {
 //		Question q = all.get(0);
 //		assertEquals("sbb가 무엇인가요?", q.getSubject());
 
-		Optional<Question> oq = this.questionRepository.findById(5);
-		if(oq.isPresent()){
-			Question q = oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
-		}
+		//findById
+//		Optional<Question> oq = this.questionRepository.findById(5);
+//		if(oq.isPresent()){
+//			Question q = oq.get();
+//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		}
+
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(13, q.getId());
+
+
 	}
 
 }
