096aa76de91e5d3d59f10bc4c185985353672f60
ghdgu <ghdguswl13@gmail.com>
2023-10-06 15:47:19 +0900

    findById test

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -15,17 +19,31 @@ class SbbApplicationTests {
 
 	@Test
 	void TestJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고싶습니다");
-		q1.setCreateData(LocalDateTime.now());
-		this.questionRepository.save(q1); //첫번째 질문 저장
+		//데이터 저장하기
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고싶습니다");
+//		q1.setCreateData(LocalDateTime.now());
+//		this.questionRepository.save(q1); //첫번째 질문 저장
+//
+//		Question q2 = new Question();
+//		q2.setSubject("스프링 부트 모델 질문입니다");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateData(LocalDateTime.now());
+//		this.questionRepository.save(q2); //두번째 질문 저장
 
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문입니다");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateData(LocalDateTime.now());
-		this.questionRepository.save(q2); //두번째 질문 저장
+		//findAll
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(6, all.size());
+//
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+		Optional<Question> oq = this.questionRepository.findById(5);
+		if(oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
