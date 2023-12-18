86facc5b3b4ff95a137fc63a868a0505c8752fb5
ghdgu <ghdguswl13@gmail.com>
2023-12-18 13:48:59 +0900

    ROOT URL설정

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -13,4 +13,11 @@ public class MainController {
         //return 값이 없을 시 오류 발생
         return "안녕하세요 sbb에 오신것을 환영합니다";
     }
+    
+    @GetMapping("/")
+    public String root(){
+        //redirect: 완전히 새로운 URL로 요청
+        //forward: 기존 요청 값들이 유지된 상태료 URL전환
+        return "redirect:question/list";
+    }
 }
